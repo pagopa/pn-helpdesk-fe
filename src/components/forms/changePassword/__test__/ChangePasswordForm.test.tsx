5bd94afd2f63f09aaf2af3d8896e2f6f462df1bb
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-11-14 18:59:21 +0100

    test adjustments

--- src/components/forms/changePassword/__test__/ChangePasswordForm.test.tsx
+++ src/components/forms/changePassword/__test__/ChangePasswordForm.test.tsx
@@ -9,7 +9,6 @@ import ChangePasswordForm from "../ChangePasswordForm";
 import { reducer } from "../../../../mocks/mockReducer";
 import userEvent from "@testing-library/user-event";
 import { act } from "react-test-renderer";
-import { Auth } from "aws-amplify";
 
 describe("ChangePasswordForm", () => {
   it("renders component", () => {
@@ -56,41 +55,5 @@ describe("ChangePasswordForm", () => {
 
     const errors = await screen.findAllByText("Password non corretta");
     expect(errors.length).toEqual(2);
-  });
-
-  it("fill inputs and click button", async () => {
-    const mockFn = jest.fn();
-    jest.mock("aws-amplify");
-    jest
-      .spyOn(Auth, "completeNewPassword")
-      .mockImplementation(mockFn)
-      .mockReturnValue(new Promise(() => "mock"));
-
-    reducer(<ChangePasswordForm />);
-    const newPassword = screen.getByLabelText("Nuova password");
-
-    const user = userEvent.setup();
-
-    await act(async () => {
-      await user.clear(newPassword);
-      await user.type(newPassword, "Test_Cognito_2.!");
-    });
-    expect(newPassword).toHaveValue("Test_Cognito_2.!");
-
-    const confirmPassword = screen.getByLabelText("Nuova password");
-
-    await act(async () => {
-      await user.clear(confirmPassword);
-      await user.type(confirmPassword, "Test_Cognito_2.!");
-    });
-    expect(confirmPassword).toHaveValue("Test_Cognito_2.!");
-
-    const button = screen.getByRole(/Button/i, {
-      name: "Cambia password",
-    });
-    expect(button).not.toBeDisabled();
-    await act(async () => {
-      await user.click(button);
-    });
   });
 });
