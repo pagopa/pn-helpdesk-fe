8d27907b8ae0b433295e8e64e361e7d712e8d868
pasqualino.cristaudo <>
2023-01-25 16:23:46 +0100

    Added Costs Form

---
+++ src/components/forms/costs/CostsForm.tsx
@@ -0,0 +1,138 @@
+import {
+  Card,
+  Typography,
+  Grid,
+  FormHelperText,
+  Button,
+} from "@mui/material";
+import React from "react";
+import {useAppDispatch} from "../../../redux/hook";
+import {Controller, useForm} from "react-hook-form";
+import {FormField} from "../../formFields/FormFields";
+
+import {fieldsCosts} from "./fields";
+import {Add} from "@mui/icons-material";
+
+const defaultFormValues: { [key: string]: any } = {
+  "dateInterval": [new Date(), new Date()]
+};
+
+export default function CostsBox({fsu: boolean}:any) {
+
+  const fields = ["selectTypeCost", "inputBaseCost", "selectCapCost", "inputAdditionalCost", "selectProductType"];
+  const dispatch = useAppDispatch();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: defaultFormValues,
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  });
+
+  const onSubmit = async (data: { [x: string]: any }) => {
+    console.log("onsubmit");
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+      <Grid item container rowSpacing={2}>
+        <Grid item container>
+          <Card
+            elevation={24}
+            sx={{
+              width: 1,
+              padding: "1rem 2rem",
+              boxShadow: "0px 3px 3px -2px ",
+              backgroundColor: "background.paper",
+            }}
+          >
+            <Grid container rowSpacing={2}>
+              <Grid item>
+                <Typography
+                  variant="h5"
+                  component="div">
+                  Nuovo costo
+                </Typography>
+              </Grid>
+              <Grid item container>
+                <Grid item container spacing={1} >
+                  {
+                    fields.map(field => (
+                      <Grid
+                        item
+                        key={fieldsCosts[field].name + "Item"}
+                        width={fieldsCosts[field].size}
+                        sx={{paddingLeft: 0}}
+                      >
+                        <Controller
+                          key={field}
+                          control={control}
+                          name={field}
+                          rules={fieldsCosts[field].rules}
+                          render={({
+                                     field: { onChange, onBlur, value, name, ref },
+                                     fieldState: { invalid, isTouched, isDirty, error },
+                                     formState,
+                                   }) => (
+                            <>
+                              <FormField
+                                error={error}
+                                key={field}
+                                field={fieldsCosts[field]}
+                                onChange={onChange}
+                                value={value}
+                              />
+                              <FormHelperText error>
+                                {errors[field] ? errors[field].message : " "}
+                              </FormHelperText>
+                            </>
+                          )}
+                        />
+                      </Grid>
+                    ))
+                  }
+                </Grid>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+        <Grid item container>
+          <Card
+            elevation={24}
+            sx={{
+              width: 1,
+              padding: "1rem 2rem",
+              boxShadow: "0px 3px 3px -2px ",
+              backgroundColor: "background.paper",
+            }}
+          >
+            <Grid container
+                  rowSpacing={2}
+                  alignItems={"center"}
+                  justifyContent="space-between">
+              <Grid item>
+                <Typography
+                  variant="h5"
+                  component="div">
+                  Costi
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button variant={"outlined"} startIcon={<Add />}>Aggiungi</Button>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+
+        <Grid item container direction="row" justifyContent="space-between">
+          <Button variant={"outlined"}>Indietro</Button>
+          <Button variant={"contained"} type={"submit"} >Avanti</Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
---
+++ src/components/forms/costs/fields.ts
@@ -0,0 +1,122 @@
+import { FieldsProps } from "../../formFields/FormFields";
+import {regex} from "../../../helpers/validations";
+import {errorMessages} from "../../../helpers/messagesConstants";
+
+
+
+export const fieldsCosts: { [key:string]: FieldsProps } = {
+  "selectTypeCost": {
+    name: "typeCost",
+    componentType: "select",
+    label: "Seleziona il tipo di costo *",
+    hidden: false,
+    size: "55%",
+    // inputProps: {width:"webkit-fill-available"},
+    // rules: {
+    //   pattern: {
+    //     value: regex.ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN,
+    //     message: errorMessages.INCORRECT,
+    //   },
+    //   required: errorMessages.REQUIRED,
+    // },
+    required: true,
+  },
+  "inputBaseCost": {
+    name: "baseCost",
+    componentType: "textfield",
+    label: "Costo base (€)",
+    hidden: false,
+    size: "45%",
+    inputProps: { minLength: 1 },
+    rules: {
+      pattern: {
+        value: regex.AMOUNT,
+        message: errorMessages.INCORRECT,
+      },
+      minLength: {
+        value: 1,
+        message: errorMessages.INCORRECT,
+      },
+      required: errorMessages.REQUIRED,
+    },
+    required: true,
+  },
+  "selectCapCost": {
+    name: "capCost",
+    componentType: "select",
+    label: "Seleziona Cap",
+    hidden: false,
+    size: "55%",
+    // inputProps: {width:"webkit-fill-available"},
+    // rules: {
+    //   pattern: {
+    //     value: regex.ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN,
+    //     message: errorMessages.INCORRECT,
+    //   },
+    //   required: errorMessages.REQUIRED,
+    // },
+    required: true,
+  },
+  "inputAdditionalCost": {
+    name: "additionalCost",
+    componentType: "textfield",
+    label: "Costo aggiuntivo per pagina (€)",
+    hidden: false,
+    size: "45%",
+    inputProps: { minLength: 1 },
+    rules: {
+      pattern: {
+        value: regex.AMOUNT,
+        message: errorMessages.INCORRECT,
+      },
+      minLength: {
+        value: 1,
+        message: errorMessages.INCORRECT,
+      },
+      required: errorMessages.REQUIRED,
+    },
+    required: true,
+  },
+  "selectZoneCost": {
+    name: "zoneCost",
+    componentType: "select",
+    label: "Seleziona Zona",
+    hidden: false,
+    size: "55%",
+    // inputProps: {width:"webkit-fill-available"},
+    // rules: {
+    //   pattern: {
+    //     value: regex.ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN,
+    //     message: errorMessages.INCORRECT,
+    //   },
+    //   required: errorMessages.REQUIRED,
+    // },
+    required: true,
+  },
+  "selectProductType": {
+    name: "productType",
+    componentType: "select",
+    label: "Seleziona la tipologia di prodotto",
+    hidden: false,
+    size: "100%",
+    // inputProps: {width:"webkit-fill-available"},
+    // inputProps: { maxLength: 11 },
+    // rules: {
+    //   pattern: {
+    //     value: regex.TAX_ID_PATTERN,
+    //     message: errorMessages.INCORRECT,
+    //   },
+    //   minLength: {
+    //     value: 11,
+    //     message: errorMessages.INCORRECT,
+    //   },
+    //   maxLength: {
+    //     value: 11,
+    //     message: errorMessages.INCORRECT,
+    //   },
+    //   required: errorMessages.REQUIRED,
+    // },
+    required: true,
+  },
+
+}
