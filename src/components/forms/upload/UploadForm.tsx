106426143377a6357c2a3a873ea22337798af39c
pasqualino.cristaudo <>
2023-01-26 11:29:10 +0100

    Updated UploadForm

--- src/components/forms/upload/UploadForm.tsx
+++ src/components/forms/upload/UploadForm.tsx
@@ -46,7 +46,6 @@ export default function UploadBox() {
               </Grid>
               <Grid item container xs={12} sm={6}>
                 <SingleFileInput
-                  id="idSingleFileInput"
                   label="Documento (richiesto)"
                   value={file}
                   accept={["image/png"]}
