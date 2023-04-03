f1b26ff3e57bfe959f5e48d165820f86c81cdfb0
cristopher.moccia <cristopher.moccia@accenture.com>
2023-04-03 21:55:09 +0200

    refactor: added enum into if condition and enum label

--- src/components/usageEstimates/EstimateStatus.tsx
+++ src/components/usageEstimates/EstimateStatus.tsx
@@ -2,15 +2,26 @@ import {Chip} from "@mui/material";
 import React from "react";
 import {EstimateStatusEnum} from "../../model";
 
+enum StatusChipLabel {
+  CREATED = "IN AGGIORNAMENTO",
+  VALIDATED = "CONSOLIDATO",
+  IN_PROGRESS = "IN CORSO",
+  ENDED = "TERMINATA"
+}
+
 
 export function EstimateStatusChip(props:{data:EstimateStatusEnum}) {
-  if (props.data === "CREATED")
-    return <Chip sx={{bgcolor: '#ffff00'}} label={"IN AGGIORNAMENTO"}/>
-  if (props.data === "VALIDATED")
-    return <Chip sx={{bgcolor: '#95f202'}}  label={"CONSOLIDATO"}/>
-  if (props.data === "IN_PROGRESS")
-    return <Chip  sx={{bgcolor: '#81d4fa'}} label={"IN CORSO"}/>
-  if (props.data === "ENDED")
-    return <Chip label={"TERMINATA"}/>
+  if (props.data === EstimateStatusEnum.Created)
+    return <Chip sx={{bgcolor: '#ffff00'}} label={StatusChipLabel.CREATED}/>
+
+  if (props.data === EstimateStatusEnum.Validated)
+    return <Chip sx={{bgcolor: '#95f202'}}  label={StatusChipLabel.VALIDATED}/>
+
+  if (props.data === EstimateStatusEnum.InProgress)
+    return <Chip  sx={{bgcolor: '#81d4fa'}} label={StatusChipLabel.IN_PROGRESS}/>
+
+  if (props.data === EstimateStatusEnum.Ended)
+    return <Chip label={StatusChipLabel.ENDED}/>
+
   return <Chip label={"-"}/>
 }
