7a32424137c793f7c6f9f38b434fb2cb15603f7b
empassaro <emanuele.passaro@nttdata.com>
2023-05-03 10:54:53 +0200

    Added warning alert in confirmation modal

--- src/components/confirmationDialog/ConfirmationProvider.tsx
+++ src/components/confirmationDialog/ConfirmationProvider.tsx
@@ -4,65 +4,76 @@ import { Options } from "./ConfirmationTypes";
 import { createContext } from "react";
 import { ContextType } from "./ConfirmationTypes";
 
-const INITIAL_OPTIONS : Options = {
-    title: "Conferma operazione",
-    message: "Sei sicuro di voler procedere?"
-}
+const INITIAL_OPTIONS: Options = {
+  title: "Conferma operazione",
+  message: "Sei sicuro di voler procedere?",
+  extraContent: <Fragment />,
+};
 
 const buildOptions = (defaultOptions: Options, options: Options) => {
-    const title = options.title || defaultOptions.title || INITIAL_OPTIONS.title;
-    const message =  options.message || defaultOptions.message || INITIAL_OPTIONS.message;
+  const title = options.title || defaultOptions.title || INITIAL_OPTIONS.title;
+  const message =
+    options.message || defaultOptions.message || INITIAL_OPTIONS.message;
+  const extraContent =
+    options.extraContent ||
+    defaultOptions.extraContent ||
+    INITIAL_OPTIONS.extraContent;
 
-    return { title,message }
-}
+  return { title, message, extraContent };
+};
 
-export const ConfirmationContext = createContext<ContextType | undefined>(undefined);
+export const ConfirmationContext = createContext<ContextType | undefined>(
+  undefined
+);
 
 type Props = {
   children: ReactNode;
-  defaultOptions?: Options
-}
-export const ConfirmationProvider = ({ children, defaultOptions = {} } : Props) => {
-    const [options, setOptions] = useState(INITIAL_OPTIONS);
-    const [resolveReject, setResolveReject] = useState<Array<any>>([]);
-    const [resolve, reject] = resolveReject;
-  
-    const confirm = useCallback((options = INITIAL_OPTIONS) => {
-      return new Promise((resolve, reject) => {
-        setOptions(options);
-        setResolveReject([resolve, reject]);
-      });
-    }, []);
-  
-    const handleClose = useCallback(() => {
-      setResolveReject([]);
-    }, []);
-  
-    const handleCancel = useCallback(() => {
-      if (reject) {
-        reject();
-        handleClose();
-      }
-    }, [reject, handleClose]);
-  
-    const handleConfirm = useCallback(() => {
-      if (resolve) {
-        resolve();
-        handleClose();
-      }
-    }, [resolve, handleClose]);
-  
-    return (
-      <Fragment>
-        <ConfirmationContext.Provider value={confirm}>
-          {children}
-        </ConfirmationContext.Provider>
-        <ConfirmationDialog
-          open={resolveReject.length === 2}
-          options={buildOptions(defaultOptions, options)}
-          onCancel={handleCancel}
-          onConfirm={handleConfirm}
-        />
-      </Fragment>
-    );
+  defaultOptions?: Options;
+};
+export const ConfirmationProvider = ({
+  children,
+  defaultOptions = {},
+}: Props) => {
+  const [options, setOptions] = useState(INITIAL_OPTIONS);
+  const [resolveReject, setResolveReject] = useState<Array<any>>([]);
+  const [resolve, reject] = resolveReject;
+
+  const confirm = useCallback((options = INITIAL_OPTIONS) => {
+    return new Promise((resolve, reject) => {
+      setOptions(options);
+      setResolveReject([resolve, reject]);
+    });
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setResolveReject([]);
+  }, []);
+
+  const handleCancel = useCallback(() => {
+    if (reject) {
+      reject();
+      handleClose();
+    }
+  }, [reject, handleClose]);
+
+  const handleConfirm = useCallback(() => {
+    if (resolve) {
+      resolve();
+      handleClose();
+    }
+  }, [resolve, handleClose]);
+
+  return (
+    <Fragment>
+      <ConfirmationContext.Provider value={confirm}>
+        {children}
+      </ConfirmationContext.Provider>
+      <ConfirmationDialog
+        open={resolveReject.length === 2}
+        options={buildOptions(defaultOptions, options)}
+        onCancel={handleCancel}
+        onConfirm={handleConfirm}
+      />
+    </Fragment>
+  );
 };
