38d7c789a8907d3a748b9a589208a8b3e1810d3b
Francesco Bianchi <f.bianchi@codermine.com>
2025-05-13 14:48:38 +0200

    fix AggregatesPage

--- src/pages/aggregates/__test__/AggregatesPage.test.tsx
+++ src/pages/aggregates/__test__/AggregatesPage.test.tsx
@@ -1,10 +1,11 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent, screen, act } from '@testing-library/react';
 import * as router from 'react-router';
 import { ConfirmationProvider } from '../../../components/confirmationDialog/ConfirmationProvider';
 import { renderWithProviders, renderWithProvidersAndPermissions } from '../../../mocks/mockReducer';
 import { Permission } from '../../../model/user-permission';
 import * as routes from '../../../navigation/router.const';
 import AggregatesPage from '../AggregatesPage';
+import apiRequests from '../../../api/apiRequests';
 
 const navigate = jest.fn();
 
@@ -12,25 +13,38 @@ describe('AggregatesPage test', () => {
   beforeEach(() => {
     // mock navigation
     jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate);
+    const apiSpyUsagePlans = jest.spyOn(apiRequests, 'getAggregates');
+    apiSpyUsagePlans.mockImplementation(() =>
+      Promise.resolve({
+        items: [],
+        lastEvaluatedId: '',
+        lastEvaluatedName: '',
+        total: 0,
+      })
+    );
   });
 
-  it('render', () => {
-    renderWithProviders(
-      <ConfirmationProvider>
-        <AggregatesPage />
-      </ConfirmationProvider>
-    );
+  it('render', async () => {
+    await act(async () => {
+      renderWithProviders(
+        <ConfirmationProvider>
+          <AggregatesPage />
+        </ConfirmationProvider>
+      );
+    });
 
     expect(screen.getByText('Gestione Aggregazioni ApiKey')).toBeInTheDocument();
   });
 
-  it('click nuova aggregazione', () => {
-    renderWithProvidersAndPermissions(
-      <ConfirmationProvider>
-        <AggregatesPage />
-      </ConfirmationProvider>,
-      [Permission.API_KEY_WRITE]
-    );
+  it('click nuova aggregazione', async () => {
+    await act(async () => {
+      renderWithProvidersAndPermissions(
+        <ConfirmationProvider>
+          <AggregatesPage />
+        </ConfirmationProvider>,
+        [Permission.API_KEY_WRITE]
+      );
+    });
 
     const nuovaAggregazioneButton = screen.getByRole('button', { name: 'Nuova aggregazione' });
     expect(nuovaAggregazioneButton).toBeInTheDocument();
