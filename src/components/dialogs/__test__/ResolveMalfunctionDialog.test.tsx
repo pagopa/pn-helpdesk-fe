b77b8f385fddd1135867b88502ad32b0ab14e3d8
Sarah Donvito <73442810+SarahDonvito@users.noreply.github.com>
2025-06-06 17:18:51 +0200

    feat/PN-15037: implement tests for create and resolve malfunction (#102)

---
+++ src/components/dialogs/__test__/ResolveMalfunctionDialog.test.tsx
@@ -0,0 +1,227 @@
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import { AxiosHeaders, AxiosResponse } from 'axios';
+import userEvent from '@testing-library/user-event';
+import { ResolveMalfunctionDialog } from '../ResolveMalfunctionDialog';
+import { FunctionalityName } from '../../../model/monitor';
+import apiRequests from '../../../api/apiRequests';
+
+const mockStore = configureStore([thunk]);
+
+const handlers = [
+  rest.post('/downtime/v1/events', (req, res, ctx) => {
+    console.log('MSW: intercepted POST!');
+    return res(ctx.status(200), ctx.json({ success: true }));
+  }),
+  rest.options('/downtime/v1/events', (req, res, ctx) => {
+    console.log('MSW: intercepted OPTIONS!');
+    return res(ctx.status(204));
+  }),
+];
+
+jest.mock('@mui/x-date-pickers', () => ({
+  ...jest.requireActual('@mui/x-date-pickers'),
+  DateTimePicker: ({ label, value, onChange }: any) => (
+    <input
+      data-testid="mock-date-picker"
+      value={value?.toString()}
+      onChange={(e) => onChange(new Date(e.target.value))}
+      placeholder={label}
+    />
+  ),
+}));
+
+jest.mock('../../../api/apiRequests', () => ({
+  __esModule: true,
+  default: {
+    createEvent: jest.fn(),
+    getPreview: () => new Promise((resolve) => resolve('Prova')),
+  },
+}));
+
+const mockedResponse: AxiosResponse<void> = {
+  data: undefined,
+  status: 200,
+  statusText: 'OK',
+  headers: new AxiosHeaders(),
+  config: {
+    headers: new AxiosHeaders(),
+  },
+};
+
+const mockedApi = apiRequests as jest.Mocked<typeof apiRequests>;
+
+const defaultProps = {
+  refreshStatus: jest.fn(),
+  setIsModalOpen: jest.fn(),
+  updateSnackbar: jest.fn<void, [AxiosResponse | undefined]>(),
+  isModalOpen: true,
+  modalPayload: {
+    functionality: 'NOTIFICATION_CREATE' as FunctionalityName,
+    status: 'KO',
+  },
+};
+
+function renderComponent() {
+  const store = mockStore({});
+  return render(
+    <Provider store={store}>
+      <ResolveMalfunctionDialog {...defaultProps} />
+    </Provider>
+  );
+}
+const server = setupServer(...handlers);
+beforeAll(() => server.listen());
+beforeEach(() => {
+  mockedApi.createEvent.mockClear();
+  mockedApi.createEvent.mockResolvedValue(mockedResponse);
+});
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('ResolveMalfunctionDialog component', () => {
+  it('render ResolveMalfunctionDialog', () => {
+    renderComponent();
+    expect(screen.getByTestId('resolve-malfunction-dialog-testid')).toBeInTheDocument();
+    expect(screen.getByText('Risolvi evento | Creazione Notifiche')).toBeInTheDocument();
+    expect(screen.getByTestId('rich-text-description')).toBeInTheDocument();
+  });
+});
+
+describe('ResolveMalfunctionDialog component - first step', () => {
+  it('setIsModalOpen is false when Annulla button is clicked', async () => {
+    renderComponent();
+    expect(screen.getByTestId('resolve-malfunction-dialog-testid')).toBeInTheDocument();
+    const cancelButton = screen.getByRole('button', { name: 'Annulla' });
+    await userEvent.click(cancelButton);
+    expect(defaultProps.setIsModalOpen).toHaveBeenCalledWith(false);
+  });
+
+  it('show error if description is not filled and go to next step when filled', async () => {
+    renderComponent();
+
+    const continueButton = screen.getByRole('button', { name: 'Continua' });
+    await userEvent.click(continueButton);
+
+    expect(
+      await screen.findByText((content) => content.includes('Inserisci informazioni aggiuntive'))
+    ).toBeInTheDocument();
+
+    const richTextContainer = screen.getByTestId('rich-text-description');
+
+    const richTextContainerEditor = richTextContainer
+      .querySelector('.ProseMirror')
+      ?.querySelector('p');
+
+    if (richTextContainerEditor) {
+      await act(async () => {
+        fireEvent.change(richTextContainerEditor, {
+          target: { textContent: 'Prova' },
+        });
+      });
+    }
+
+    await userEvent.click(continueButton);
+    expect(screen.getByTestId('resolve-malfunction-dialog-testid')).toBeInTheDocument();
+    expect(screen.getByText('Anteprima documento | Creazione Notifiche')).toBeInTheDocument();
+  });
+});
+
+describe('ResolveMalfunctionDialog component - second step', () => {
+  it('check preview is showed', async () => {
+    renderComponent();
+
+    const continueButton = screen.getByRole('button', { name: 'Continua' });
+    const richTextContainer = screen.getByTestId('rich-text-description');
+
+    const richTextContainerEditor = richTextContainer
+      .querySelector('.ProseMirror')
+      ?.querySelector('p');
+
+    if (richTextContainerEditor) {
+      await act(async () => {
+        fireEvent.change(richTextContainerEditor, {
+          target: { textContent: 'Prova' },
+        });
+      });
+    }
+
+    await userEvent.click(continueButton);
+    expect(screen.getByTestId('resolve-malfunction-dialog-testid')).toBeInTheDocument();
+    expect(screen.getByText('Anteprima documento | Creazione Notifiche')).toBeInTheDocument();
+
+    expect(await screen.findByTitle('PDF Viewer')).toBeTruthy();
+  });
+
+  it('go back to first step when Indietro button is clicked', async () => {
+    renderComponent();
+
+    const continueButton = screen.getByRole('button', { name: 'Continua' });
+    const richTextContainer = screen.getByTestId('rich-text-description');
+
+    const richTextContainerEditor = richTextContainer
+      .querySelector('.ProseMirror')
+      ?.querySelector('p');
+
+    if (richTextContainerEditor) {
+      await act(async () => {
+        fireEvent.change(richTextContainerEditor, {
+          target: { textContent: 'Prova' },
+        });
+      });
+    }
+
+    await userEvent.click(continueButton);
+
+    expect(screen.getByTestId('resolve-malfunction-dialog-testid')).toBeInTheDocument();
+    const backButton = screen.getByRole('button', { name: 'Indietro' });
+    await userEvent.click(backButton);
+    expect(screen.getByTestId('rich-text-description')).toBeInTheDocument();
+  });
+
+  it('show error if checkbox is not checked and call api when checked', async () => {
+    renderComponent();
+
+    const continueButton = screen.getByRole('button', { name: 'Continua' });
+    const richTextContainer = screen.getByTestId('rich-text-description');
+
+    const richTextContainerEditor = richTextContainer
+      .querySelector('.ProseMirror')
+      ?.querySelector('p');
+
+    if (richTextContainerEditor) {
+      await act(async () => {
+        fireEvent.change(richTextContainerEditor, {
+          target: { textContent: 'Prova' },
+        });
+      });
+    }
+
+    await userEvent.click(continueButton);
+
+    const insertButton = screen.getByRole('button', { name: 'Conferma e Pubblica' });
+    await userEvent.click(insertButton);
+
+    expect(
+      await screen.findByText((content) => content.includes('Questo campo è obbligatorio'))
+    ).toBeInTheDocument();
+
+    const checkboxInput = screen
+      .getByTestId('checkbox')
+      .querySelector('input[type="checkbox"]') as HTMLInputElement;
+
+    await userEvent.click(checkboxInput);
+    await waitFor(() => {
+      expect(checkboxInput).toBeChecked();
+    });
+    await userEvent.click(insertButton);
+
+    await waitFor(() => {
+      expect(mockedApi.createEvent).toHaveBeenCalledTimes(1);
+    });
+  });
+});
